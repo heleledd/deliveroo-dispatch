f0034dd6295f4757cb5bf450fac682f606f1d82f
heleledd <157613131+heleledd@users.noreply.github.com>
2025-11-12 16:17:40 +0000

    riders turn grey when unavailable

--- src/components/game/Riders/RiderCard.jsx
+++ src/components/game/Riders/RiderCard.jsx
@@ -1,6 +1,6 @@
 export default function RiderCard(props) {
     return (
-        <div className='rider-card'>
+        <div className={`rider-card ${!props.isAvailable ? 'unavailable' : ''}`}>
             <div className="rider-image-container">
                 <img className='rider-image' src={props.profileImg.src} alt={props.profileImg.alt} />
             </div>
--- src/data/jobs.js
+++ src/data/jobs.js
@@ -3,31 +3,31 @@ const jobs = [
         id: 1,
         description: "Deliver sushi to City Center", 
         distance: 5, 
-        pay: 12 
+        pay: 1 
     },
     { 
         id: 2, 
         description: "Pick up pizza from Pizza Express", 
         distance: 3, 
-        pay: 8 
+        pay: 2.91 
     },
     { 
         id: 3, 
         description: "Deliver groceries to Cathays", 
         distance: 7, 
-        pay: 15 
+        pay: 4 
     },
     { 
         id: 4, 
         description: "Collect order from BurgerHub", 
         distance: 4, 
-        pay: 10 
+        pay: 2.73 
     },
     { 
         id: 5, 
         description: "Deliver coffee to Abacws", 
         distance: 2, 
-        pay: 6 
+        pay: 1.75 
     },
 ];
 
